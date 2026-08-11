2b940e2237735f4e2100de84c3f68149d3192a7d
jotavgalves <jvgacontato@gmail.com>
2026-08-11 16:35:13 -0300

    Use shared Pages helper module outside routes

--- functions/api/admin/logout.js
+++ functions/api/admin/logout.js
@@ -1,4 +1,4 @@
-import { json, sessionCookie } from '../../_lib.js';
+import { json, sessionCookie } from '../../../src/functions-lib.js';
 export async function onRequestPost({ request }) {
   return json({ ok:true }, 200, { 'set-cookie': sessionCookie(request, '', 0) });
 }
